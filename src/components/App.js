de7c87991953267c7b7e9a3e93d6fe268cdfa92d
breadman <swo98@naver.com>
2023-01-24 00:53:43 +0900

    auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
 import React, { useState } from "react";
-import AppRouter from "components/Router";
+import AppRouter from "components/AppRouter";
 import { authService } from "fbase";
+import { useEffect } from "react";
+import Auth from "routes/Auth";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      // 로그인 상태/로그아웃 상태 분기해줄 수 있음 onAuthStateChanged()
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "초기화중..."}
+      {/* {isLoggedIn ? "로그인됨" : <Auth />} */}
       <footer>&copy; {new Date().getFullYear()} Bwitter</footer>
     </>
   );
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,7 +1,74 @@
-import React from "react";
+import { authService } from "fbase";
+import React, { useState } from "react";
 
 function Auth() {
-  return <>Auth</>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [newAccount, setNewAccount] = useState(true);
+  const [error, setError] = useState("");
+  const onChange = (event) => {
+    //const { name, value } = event.target;
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      //email input이면
+      setEmail(value); // input value를 email로하겠다.
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      if (newAccount) {
+        // create account
+        await authService.createUserWithEmailAndPassword(email, password);
+      } else {
+        // log in
+        await authService.signInWithEmailAndPassword(email, password);
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+  const toggleAccount = () => {
+    setNewAccount((cur) => !cur);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange}
+        ></input>
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange}
+        ></input>
+        <input
+          type="submit"
+          value={newAccount ? "Create Account" : "Log In"}
+        ></input>
+        <p>{error}</p>
+      </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? "Sign in" : "Create account"}
+      </span>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
+  );
 }
 
 export default Auth;
