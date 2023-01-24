3cf8296a0c8c11888928b7c6b8b6f3fcef0bdcd1
breadman <swo98@naver.com>
2023-01-24 16:49:48 +0900

    db service

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,14 @@ import { useEffect } from "react";
 function App() {
   const [init, setInit] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       // 로그인 상태/로그아웃 상태 분기해줄 수 있음 onAuthStateChanged()
+      // 로그인 했을 떄, user의 정보가 여기에 있음
       if (user) {
         setIsLoggedIn(true);
+        setUserObj(user);
       } else {
         setIsLoggedIn(false);
       }
@@ -19,7 +22,11 @@ function App() {
   }, []);
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "초기화중..."}
+      {init ? (
+        <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
+      ) : (
+        "초기화중..."
+      )}
       {/* {isLoggedIn ? "로그인됨" : <Auth />} */}
       <footer>&copy; {new Date().getFullYear()} Bwitter</footer>
     </>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,23 +2,36 @@ import { dbService } from "fbase";
 import React, { useState } from "react";
 import { useEffect } from "react";
 
-function Home() {
+function Home({ userObj }) {
   const [bweet, setBweet] = useState("");
   const [bweets, setBweets] = useState([]);
+  // console.log(userObj.uid);
 
-  const getBweets = async () => {
-    const dbBweets = await dbService.collection("bweets").get();
-    dbBweets.forEach((docs) => {
-      const bweetsObj = {
-        ...docs.data(),
-        id: docs.id,
-      };
-      setBweets((prev) => [bweetsObj, ...prev]);
-    });
-  };
+  // // collection에 있는 것을 get 하는 방식
+  // // -> realtime이 아니라 이미 컬렉션에 있는 것을 가져오는 방식 -> 새로고침으로 렌더링필요
+  // const getBweets = async () => {
+  //   const dbBweets = await dbService.collection("bweets").get();
+  //   dbBweets.forEach((docs) => {
+  //     const bweetsObj = {
+  //       ...docs.data(),
+  //       id: docs.id,
+  //     };
+  //     setBweets((prev) => [bweetsObj, ...prev]);
+  //   });
+  // };
 
   useEffect(() => {
-    getBweets();
+    //getBweets();
+    dbService
+      .collection("bweets")
+      .orderBy("createdAt", "desc")
+      .onSnapshot((snapshot) => {
+        const bweetsArray = snapshot.docs.map((docs) => ({
+          id: docs.id,
+          ...docs.data(),
+        }));
+        setBweets(bweetsArray);
+      });
   }, []);
   // useEffect에 async/awit 쓸거면 함수 밖에 써준거를
   // 안에서 호출하던지
@@ -30,6 +43,7 @@ function Home() {
       //bweet:bweet,
       bweet,
       createdAt: Date.now(),
+      creatorId: userObj.uid,
     });
     setBweet("");
   };
@@ -52,6 +66,8 @@ function Home() {
           />
           <input type="submit" value="Bweet" />
         </form>
+
+        {/* 화면에 텍스트 렌더링 */}
         <div>
           {bweets.map((bw) => (
             <div key={bw.id}>
